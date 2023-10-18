b81212b21f6e409054b876f32666533fc2502da4
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-10-18 17:34:12 +0200

    Add font and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Open Sans', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'Arial'],
+    },
+    colors: {
+      "my-orange": "#FF8001",
+      "my-purple": "#2E0B51",
+      "my-dark":"#21262D",
+      "my-darker":"#121519",
+      "my-light":"#EDE9E9",
+      "my-lighter": "#F4F4F4",
+
+    },
     extend: {},
   },
   plugins: [],
