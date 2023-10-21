3997dee5498bdd6af50b532f019789862e07fd34
gf-rog <gf.rogozinski@gmail.com>
2023-10-21 12:28:37 +0200

    Add CORS support

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+
 import usersRouter from './routes/usersRoute';
 
 const app: Express = express();
 const port: number = 5000;
+
+const corsOptions = {
+  origin: "http://localhost:4173",
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Mercury Project");
