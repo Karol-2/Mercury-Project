b647bf354d63ec5f4b1a7eb52a3beb9070741006
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-10-27 22:10:14 +0200

    Add page not found

--- frontend/src/layout/Banner.tsx
+++ frontend/src/layout/Banner.tsx
@@ -3,13 +3,15 @@ import LogoSVG from "/logo.svg";
 
 function Banner() {
   return (
-    <Link to="/" className="flex flex-row justify-center bg-my-dark py-2">
-      <img src={LogoSVG} alt="Mercury Logo" className=" h-32 w-32 pr-5 " />
-      <span className=" self-center text-my-orange font-bold text-3xl md:text-5xl">
-        {" "}
-        Mercury
-      </span>
-    </Link>
+    <div className=" bg-my-dark py-2">
+      <Link to="/" className="flex flex-row justify-center">
+        <img src={LogoSVG} alt="Mercury Logo" className=" h-32 w-32 pr-5 " />
+        <span className=" self-center text-my-orange font-bold text-3xl md:text-5xl">
+          {" "}
+          Mercury
+        </span>
+      </Link>
+    </div>
   );
 }
 
