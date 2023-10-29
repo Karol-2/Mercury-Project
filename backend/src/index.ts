13c986976a0044d8fb9e0c48ad32cb0994c54bea
Piterson25 <piotrmaszczak25@gmail.com>
2023-10-29 18:08:36 +0100

    Fix cors not allowing backend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const app: Express = express();
 const port: number = 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ['http://localhost:5000', 'http://localhost:5173'],
   optionsSuccessStatus: 200
 }
 
