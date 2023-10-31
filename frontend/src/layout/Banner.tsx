72c6b273e1193dc66b31e4e803d19a41e0ccc95b
Karol Krawczykiewicz <26karol26@gmail.com>
2023-10-31 18:00:04 +0100

    Change buttons

--- frontend/src/layout/Banner.tsx
+++ frontend/src/layout/Banner.tsx
@@ -3,7 +3,7 @@ import LogoSVG from "/logo.svg";
 
 function Banner() {
   return (
-    <div className=" bg-my-dark py-2">
+    <div className=" bg-my-dark py-2" id="banner">
       <Link to="/" className="flex flex-row justify-center">
         <img src={LogoSVG} alt="Mercury Logo" className=" h-32 w-32 pr-5 " />
         <span className=" self-center text-my-orange font-bold text-3xl md:text-5xl">
