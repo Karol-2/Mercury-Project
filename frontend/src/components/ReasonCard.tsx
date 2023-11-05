0bc5c1b7fa7b6fcd45e905f839468b4ca1e1bbf5
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-05 19:38:44 +0100

    Clean techonolgies panel

---
+++ frontend/src/components/ReasonCard.tsx
@@ -0,0 +1,18 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface IReasonCard{
+    icon: IconProp,
+    title: string,
+}
+
+function ReasonCard(reasonsData: IReasonCard) {
+    return (
+      <div className="hover:bg-my-light hover:text-my-dark p-5 rounded-xl text-center clickable cursor-default my-5">
+        <FontAwesomeIcon icon={reasonsData.icon} className="text-my-orange text-8xl mb-5" />
+        <p className="font-semibold text-lg">{reasonsData.title}</p>
+      </div>
+    );
+  }
+
+export default ReasonCard
--- frontend/src/layout/Reasons.tsx
+++ frontend/src/layout/Reasons.tsx
@@ -4,71 +4,35 @@ import {
   faUsers,
   faGlobe,
 } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { motion } from "framer-motion";
+import ReasonCard from "../components/ReasonCard";
 
 export default function Reasons() {
-
-
   return (
-    
-
-      <section className="  bg-my-purple text-my-light p-10">
-        <h1 className=" text-center text-5xl font-bold">
-          Why Should I Use Mercury?
-        </h1>
-        <motion.div
-       initial={{ x: -200 }}
-       whileInView={{ x: 0 }}
-       viewport={{ once: true }}
-       transition={{ duration: 1 }}
-    >
-        <div className="flex flex-col md:grid md:grid-cols-4 md:gap-x-50  justify-evenly mt-5">
-          <div
-            className=" hover:bg-my-light hover:text-my-dark p-5 rounded-xl
-           text-center clickable cursor-default my-5"
-          >
-            <FontAwesomeIcon
-              icon={faShield}
-              className=" text-my-orange text-8xl mb-5"
-            />
-            <p className=" font-semibold text-lg">Privacy and Security</p>
-          </div>
-          <div
-            className=" hover:bg-my-light hover:text-my-dark p-5 rounded-xl
-           text-center clickable cursor-default my-5"
-          >
-            <FontAwesomeIcon
-              icon={faDesktop}
-              className=" text-my-orange text-8xl mb-5"
-            />
-            <p className=" font-semibold text-lg">
-              Cross-Platform Compatibility
-            </p>
-          </div>
-          <div
-            className=" hover:bg-my-light hover:text-my-dark p-5 rounded-xl
-           text-center clickable cursor-default my-5"
-          >
-            <FontAwesomeIcon
-              icon={faUsers}
-              className=" text-my-orange text-8xl mb-5"
-            />
-            <p className=" font-semibold text-lg">User-Friendly Interface</p>
-          </div>
-          <div
-            className=" hover:bg-my-light hover:text-my-dark p-5 rounded-xl
-           text-center clickable cursor-default my-5"
-          >
-            <FontAwesomeIcon
-              icon={faGlobe}
-              className=" text-my-orange text-8xl mb-5"
-            />
-            <p className=" font-semibold text-lg">Global Reach</p>
-          </div>
+    <section className="bg-my-purple text-my-light p-10">
+      <h1 className="text-center text-5xl font-bold">
+        Why Should I Use Mercury?
+      </h1>
+      <motion.div
+        initial={{ x: -200 }}
+        whileInView={{ x: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+        <div className="flex flex-col md:grid md:grid-cols-4 md:gap-x-50 justify-evenly mt-5">
+          <ReasonCard icon={faShield} title="Privacy and Security" />
+          <ReasonCard
+            icon={faDesktop}
+            title="Cross-Platform Compatibility"
+          />
+          <ReasonCard
+            icon={faUsers}
+            title="User-Friendly Interface"
+          />
+          <ReasonCard icon={faGlobe} title="Global Reach" />
         </div>
-        </motion.div>
-      </section>
-    
+      </motion.div>
+    </section>
   );
 }
