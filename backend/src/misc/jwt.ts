ad6c4157e04ad9f243e2b43c9e6adc621ff32962
gf-rog <gf.rogozinski@gmail.com>
2023-11-19 18:40:28 +0100

    Add basic JWT token functions

---
+++ backend/src/misc/jwt.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export interface JWTRequest extends Request {
+  token?: jwt.Jwt
+}
+
+export function generateAccessToken(userId: string) {
+  return jwt.sign({ userId }, process.env.TOKEN_SECRET!, { expiresIn: 900 })
+}
+
+export async function authenticateToken(
+  req: JWTRequest, res: Response, next: NextFunction
+) {
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
+  if (token == null) {
+    return res.status(401).send({ status: "unauthorized" })
+  }
+
+  try {
+    const verifiedToken = await jwt.verify(token, process.env.TOKEN_SECRET!)
+    req.token = verifiedToken as jwt.Jwt
+    next()
+  } catch (e) {
+    return res.status(403).json({ status: "forbidden" })
+  }
+}
