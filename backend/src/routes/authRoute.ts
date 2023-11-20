bc6e2de4f4fcc007dd7198b042dcf134f2410dfc
gf-rog <gf.rogozinski@gmail.com>
2023-11-20 15:52:36 +0100

    Add refresh token endpoint

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,17 +1,34 @@
-import { Router, Request } from "express";
+import { Router, Request, Response } from "express";
 
 import driver from "../driver/driver";
+import { generateAccessToken, generateRefreshToken } from "../misc/jwt";
 import {
-  generateAccessToken,
-  generateRefreshToken,
-} from "../misc/jwt";
-import { CustomResponse, JWTResponse, AuthResponse, ErrorResponse } from "../models/Response";
+  CustomResponse,
+  JWTResponse,
+  AuthResponse,
+  ErrorResponse,
+} from "../models/Response";
 
-const authRouter = Router()
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const authRouter = Router();
 
 type TokenErrorResponse = CustomResponse<
   JWTResponse | AuthResponse | ErrorResponse
 >;
+
+function generateTokens(res: TokenErrorResponse, userId: string) {
+  const token = generateAccessToken(userId);
+  const refreshToken = generateRefreshToken(userId);
+
+  res.cookie("refreshToken", refreshToken, {
+    secure: true,
+    httpOnly: true,
+    maxAge: 1209600,
+  });
+
+  return token;
+}
 
 authRouter.post("/login", async (req: Request, res: TokenErrorResponse) => {
   const { mail, password } = req.body;
@@ -35,19 +52,31 @@ authRouter.post("/login", async (req: Request, res: TokenErrorResponse) => {
       return res.status(401).json({ status: "unauthorized" });
     }
 
-    const token = generateAccessToken(user.id);
-    const refreshToken = generateRefreshToken(user.id);
-
-    res.cookie("refreshToken", refreshToken, {
-      secure: true,
-      httpOnly: true,
-      maxAge: 1209600,
-    });
+    const token = generateTokens(res, user.id);
     res.json({ status: "ok", token });
   } catch (err) {
     console.log("Error:", err);
     return res.status(404).json({ status: "error", errors: err as object });
   }
-})
+});
+
+authRouter.post("/refresh", async (req: Request, res: TokenErrorResponse) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return res.status(403).json({ status: "forbidden" });
+  }
+
+  try {
+    const verifiedToken = jwt.verify(
+      refreshToken,
+      process.env.TOKEN_SECRET!,
+    ) as JwtPayload;
+
+    const token = generateTokens(res, verifiedToken.userId);
+    return res.json({ status: "ok", token });
+  } catch (e) {
+    return res.status(403).json({ status: "forbidden" });
+  }
+});
 
 export default authRouter;
