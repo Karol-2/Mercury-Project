112ee4a74ce2466077629dff4fbee20c34d12f78
j.krol <j.krol@dev.polskiepolisy.pl>
2023-11-20 18:23:03 +0100

    Add last endpoint for httpServer to encode user data

--- backend/src/httpServer.ts
+++ backend/src/httpServer.ts
@@ -19,7 +19,7 @@ app.post("/meeting", async (req, res) => {
             RETURN m
         `, {ownerId, guestId});
         await session.close();
-        if (newMeetingRequest.records.length == 0) {
+        if (newMeetingRequest.records.length === 0) {
             return res.status(404).json({ status: "error", errors: {message: "Cannot create new meeting"} });
         }
         const meetingId = newMeetingRequest.records[0].get(0).identity.low;
@@ -35,4 +35,24 @@ app.post("/decode", (req, res) => {
     const {token} = req.body as {token: string};
     const decodedData = verify(token, linkSecret);
     return res.json({decodedData});
-}); 
+});
+
+app.get("/guest/:guestId", async (req, res) => {
+    try {
+        const guestId = Number(req.params.guestId);
+        const session = driver.session();
+        const guestRequest = await session.run(`
+            MATCH (u:User) WHERE id(u)=$guestId RETURN u
+        `,{guestId});
+        await session.close();
+        if (guestRequest.records.length === 0) {
+            return res.status(404).json({ status: "error", errors: {message: "User does not exist"} });
+        }
+        const {identity, properties} = guestRequest.records[0].get(0);
+        const token = sign({id: identity.low, ...properties}, linkSecret);
+        return res.json({token});
+    } catch (err) {
+        console.log("Error:", err);
+        return res.status(404).json({ status: "error", errors: err as object });
+    }
+});
