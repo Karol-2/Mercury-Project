45cf829ad196811c58a2b6c602991a4a340d2f07
j.krol <j.krol@dev.polskiepolisy.pl>
2023-11-24 18:03:18 +0100

    Add initial UUIDs for users

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,12 +1,12 @@
 import Chat from "./Chat";
 
 export default interface User {
-  password: string;
   first_name: string;
   last_name: string;
   country: string;
   profile_picture: string;
   mail: string;
+  password: string;
   friend_ids?: number[];
   chats?: Chat[];
 }
