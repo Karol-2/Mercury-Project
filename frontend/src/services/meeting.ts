fbf6fdead5decbc98f877bc44850d3aa1293048d
j.krol <j.krol@dev.polskiepolisy.pl>
2023-11-24 17:22:24 +0100

    Test socket server connection successfully

--- frontend/src/services/meeting.ts
+++ frontend/src/services/meeting.ts
@@ -2,8 +2,23 @@ class MeetingService {
     
     private url: string = "http://localhost:5000";
 
-    async createMeetingWithToken() {
-        
+    async createMeetingWithToken(ownerId: string, guestId: string) {
+        const endpoint = this.url + "/meeting"
+        try {
+            const response = await fetch(endpoint, {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ ownerId, guestId }), 
+            });
+            const data = await response.json();
+            const {token} = data;
+            return token;
+        } catch (error) {
+            console.error("Error ocurred during fetch data:", error);
+            throw error;
+        }
     }
 
 }
