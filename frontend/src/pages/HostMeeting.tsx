681c21072c9e35ea2bace775853e2089d5bfe4ad
j.krol <j.krol@dev.polskiepolisy.pl>
2023-11-27 12:13:38 +0100

    Implement creating peer connection and remote stream

--- frontend/src/pages/HostMeeting.tsx
+++ frontend/src/pages/HostMeeting.tsx
@@ -6,6 +6,8 @@ import { useSearchParams } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import updateCallStatus from "../redux/actions/updateCallStatus";
 import addStream from "../redux/actions/addStream";
+import socketConnection from "../webSocket/socketConnection";
+import createPeerConnection from "../webRTC/createPeerConnection";
 function HostMeeting() {
     const dispatch = useDispatch();
     const callStatus = useSelector((state: RootState) => state.callStatus);
@@ -15,13 +17,41 @@ function HostMeeting() {
     const smallFeedEl = useRef<HTMLVideoElement>(null);
     const largeFeedEl = useRef<HTMLVideoElement>(null);
     const uuidRef = useRef<any>(null);
-    
+    useEffect(() => {
+        const fetchMedia = async () => {
+            const constraints = {
+                video: true,
+                audio: true
+            }
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                dispatch(updateCallStatus("haveMedia", true));
+                dispatch(addStream("localStream", stream));
+                const {peerConnection, remoteStream} = await createPeerConnection(addIce);
+                dispatch(addStream("remote1", remoteStream, peerConnection));
+                if (largeFeedEl.current) {
+                    largeFeedEl.current.srcObject = remoteStream;
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchMedia();
+    }, []);
+    const addIce = (iceC: RTCPeerConnectionIceEvent) => {
+        const socket = socketConnection(searchParams.get("token")!);
+        socket.emit("iceToServer", {
+            iceC,
+            who: "owner",
+            uuid: uuidRef.current
+        });
+    }
     return (
         <>
             <Navbar />
             <div>
-                <video id="large-feed" autoPlay controls playsInline></video>
-                <video id="small-feed" autoPlay controls playsInline></video>
+                <video id="large-feed" ref={largeFeedEl} autoPlay controls playsInline></video>
+                <video id="small-feed" ref={smallFeedEl} autoPlay controls playsInline></video>
             </div>
             <Footer />
         </>
