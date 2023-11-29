751931e9395f937dc989a6f55b1ce341a74276bc
gf-rog <gf.rogozinski@gmail.com>
2023-11-29 10:58:47 +0100

    Add error handling

--- frontend/src/components/RegisterBox.tsx
+++ frontend/src/components/RegisterBox.tsx
@@ -2,6 +2,42 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FrontendUser } from "../models/user.model";
 
+const mailRegex = new RegExp("w+@w+[.]w+");
+
+type ErrorHandlers = Record<keyof FrontendUser, (value: string) => string>;
+const errorHandlers: ErrorHandlers = {
+  first_name: (v) => {
+    if (v.length < 2) {
+      return "First name should be at least two characters long";
+    }
+    return "";
+  },
+  last_name: (v) => {
+    if (v.length < 2) {
+      return "Last name should be at least two characters long";
+    }
+    return "";
+  },
+  country: (_v) => {
+    return "";
+  },
+  profile_picture: (_v) => {
+    return "";
+  },
+  mail: (v) => {
+    if (!mailRegex.test(v)) {
+      return "Incorrect mail format";
+    }
+    return "";
+  },
+  password: (v) => {
+    if (v.length < 8) {
+      return "Password should be at least eight characters long";
+    }
+    return "";
+  },
+};
+
 function RegisterBox() {
   const [user, setUser] = useState<Partial<FrontendUser>>({});
   const [errors, setErrors] = useState<Partial<FrontendUser>>({});
@@ -42,7 +78,24 @@ function RegisterBox() {
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    console.log(user);
+
+    const errors = Object.fromEntries(
+      Object.entries(errorHandlers).map(([key, handler]) => {
+        const formValue = user[key as keyof FrontendUser];
+        if (!formValue) {
+          return [key, "Required"];
+        }
+
+        return [key, handler(formValue)];
+      }),
+    );
+    const noErrors = Object.values(errors).every((v) => v == "");
+
+    if (noErrors) {
+      console.log(user);
+    } else {
+      setErrors(errors);
+    }
   };
 
   return (
