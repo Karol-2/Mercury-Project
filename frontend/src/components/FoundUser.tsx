21d045cca5f8f90a2785d255ca0f5b65bd8577de
Karol Krawczykiewicz <26karol26@gmail.com>
2023-11-29 19:28:49 +0100

    Style searching components

--- frontend/src/components/FoundUser.tsx
+++ frontend/src/components/FoundUser.tsx
@@ -1,17 +1,20 @@
 import User from "../models/User";
 
 interface FoundUserInterface{
-    user: User
+    user: User,
+    key: string;
 }
 
 function FoundUser(props: FoundUserInterface) {
 
   return (
-    <div className="">
-        <img src={props.user.profile_picture}></img>
-     <p> {props.user.first_name + " "+ props.user.last_name}</p>
-     <p> {props.user.country}</p>
-     <button> Add Friend</button>
+    <div className="flex flex-row bg-my-dark p-5 rounded-lg mt-5">
+        <img src={props.user.profile_picture} className=" rounded-full w-40 h-40 border-my-orange border-4" />
+        <div className=" ml-5 flex flex-col justify-evenly">
+          <p className=" font-semibold text-3xl"> {props.user.first_name + " "+ props.user.last_name}</p>
+          <p> {props.user.country}</p>
+          <button className=" btn small bg-my-purple text-xs"> Add Friend</button>
+        </div>
     </div>
   );
 }
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -8,7 +8,11 @@ function SearchPage() {
   const [searchState, setSearchState] = useState("");
   const [usersFound, setUsersFound] = useState([]);
 
-  const handleSearch = async() => {
+  const handleSearch = async(e: { preventDefault: () => void; }) => {
+    if(searchState.trim() === ""){
+      return
+    }
+    e.preventDefault();
     const response = await dataService.fetchData(`/users/search?q=${searchState}`,"GET")
     setUsersFound(response.users)
     
@@ -16,15 +20,18 @@ function SearchPage() {
   return (
     <>
       <Navbar />
-      <section className=" ">
-        <div className="flex flex-row w-full" >
-          <input type="text" placeholder="Enter your friend's name..." className="form-input text-my-darker" 
-          onChange={e => setSearchState(e.target.value)}></input>
-          <button type="submit" className="btn small bg-my-purple" onClick={handleSearch}>Search</button>
+      <section className=" min-h-screen mx-50 lg:mx-72 ">
+        <div>
+          <form className="flex flex-row max-w-3xl w-full mt-5" onSubmit={handleSearch} >
+            <input type="text" placeholder="Enter your friend's name..." className="form-input text-my-darker" 
+            onChange={e => setSearchState(e.target.value)}></input>
+            <button type="submit" className="btn small bg-my-purple ml-5">Search</button>
+          </form>
         </div>
         <div>
           {
-            usersFound.map((user) => ( <FoundUser user={user[0]}></FoundUser>))
+            usersFound && usersFound.length > 0
+             && usersFound.map((user, index) => ( <FoundUser user={user[0]} key={String(index)} />))
           }
         </div>
       </section>
