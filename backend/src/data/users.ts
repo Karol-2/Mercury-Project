a9b9444f75d4cbda9ccc8681230ca125cd36d887
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2023-11-30 21:18:34 +0100

    Remove politictians and add new uses

--- backend/src/data/users.ts
+++ backend/src/data/users.ts
@@ -1,14 +1,14 @@
 const userData = [
   {
     "id": "e023f020-1f16-49f7-97eb-0efe144d42f0",
-    "first_name": "Joe",
-    "last_name": "Biden",
-    "country": "USA",
+    "first_name": "Iga",
+    "last_name": "Swiatek",
+    "country": "Poland",
     "profile_picture":
-      "https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/ea/ec/36/eaec3686-c87d-1957-8d44-c6b8addda35a/pr_source.png/256x256bb.jpg",
-    "mail": "joe.biden@whitehouse.gov",
-    "password": "sleepyjoe",
-    "friend_ids": [2],
+      "https://img.wprost.pl/img/iga-swiatek/a3/fc/65be6559a7e0b07bd1acc56c2765.jpeg",
+    "mail": "1ga@wta.com",
+    "password": "numberone",
+    "friend_ids": [3,2],
     "chats": [],
   },
   {
@@ -25,14 +25,14 @@ const userData = [
   },
   {
     "id": "f3827527-4ecb-4c6c-a819-ba4d644704de",
-    "first_name": "Andrzej",
-    "last_name": "Duda",
+    "first_name": "Robert",
+    "last_name": "Lewandowski",
     "country": "Poland",
     "profile_picture":
-      "https://bi.im-g.pl/im/sonar/politicians/D/duda_andrzej_GxpBgRH.jpg",
-    "mail": "andrzej.dudu@prezydent.pl",
-    "password": "Dluuugopis2015",
-    "friend_ids": [1],
+      "https://cdn.galleries.smcloud.net/t/galleries/gf-qvgn-mMAq-ABgC_robert-lewandowski-994x828.jpg",
+    "mail": "lewy.robi@pzpn.pl",
+    "password": "Euro2012",
+    "friend_ids": [1,0],
     "chats": [],
   },
   {
@@ -81,6 +81,286 @@ const userData = [
     "friend_ids": [3],
     "chats": [],
   },
+  {
+    "id": "8ec319bf-6dc4-456a-8976-a57c2aecb5d2",
+    "first_name": "Selena",
+    "last_name": "Halso",
+    "country": "Morocco",
+    "profile_picture": "https://images.healthshots.com/healthshots/en/uploads/2020/12/08182549/positive-person.jpg",
+    "mail": "shalso0@nifty.com",
+    "password": "Synergized",
+    "friend_ids": [
+  1,15,18
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "48c1fc1d-3a3d-48c5-9f51-4ad49d1a617a",
+    "first_name": "Silva",
+    "last_name": "Hudghton",
+    "country": "China",
+    "profile_picture": "https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg",
+    "mail": "shudghton1@geocities.com",
+    "password": "Cloned",
+    "friend_ids": [
+  2,3,26
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "bcd120cc-446a-4686-9fe8-e7e8c85ae8b2",
+    "first_name": "Bronson",
+    "last_name": "Conford",
+    "country": "Russia",
+    "profile_picture": "https://static01.nyt.com/images/2022/05/19/opinion/firstpersonPromo/firstpersonPromo-superJumbo.jpg",
+    "mail": "bconford2@wikimedia.org",
+    "password": "heuristic",
+    "friend_ids": [
+  1,2,14,17,13,19,15
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "885b500e-43f6-477e-bcde-d19e0945cbdc",
+    "first_name": "Carole",
+    "last_name": "Ruckman",
+    "country": "Finland",
+    "profile_picture": "https://static01.nyt.com/images/2022/05/19/opinion/firstpersonPromo/firstpersonPromo-superJumbo.jpg",
+    "mail": "cruckman3@archive.org",
+    "password": "core",
+    "friend_ids": [
+  0,11,4,15,18,13
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "cadde518-aac1-489e-987e-eda0e5dd7826",
+    "first_name": "Gian",
+    "last_name": "Blasik",
+    "country": "China",
+    "profile_picture": "https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg",
+    "mail": "gblasik4@umich.edu",
+    "password": "executive",
+    "friend_ids": [
+  15,16,17,18
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "8a7063a4-f0ff-4cb1-add5-6fa972e14e58",
+    "first_name": "Lyman",
+    "last_name": "Truman",
+    "country": "Portugal",
+    "profile_picture": "https://t3.ftcdn.net/jpg/03/02/88/46/360_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp.jpg",
+    "mail": "ltruman5@hc360.com",
+    "password": "full-range",
+    "friend_ids": [
+  14,17,13,15,21
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "2821b350-23ab-4efc-826e-2d0dc0470b87",
+    "first_name": "Brooke",
+    "last_name": "McMurty",
+    "country": "Greenland",
+    "profile_picture": "https://t3.ftcdn.net/jpg/03/02/88/46/360_F_302884605_actpipOdPOQHDTnFtp4zg4RtlWzhOASp.jpg",
+    "mail": "bmcmurty6@photobucket.com",
+    "password": "Universal",
+    "friend_ids": [
+  22,23,24
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "1bc3c96c-1f5e-4114-a899-faccce69acad",
+    "first_name": "Olwen",
+    "last_name": "Helbeck",
+    "country": "China",
+    "profile_picture": "https://www.westernunion.com/staticassets/content/dam/wu/jm/responsive/send-money-in-person-from-jamaica-resp.png",
+    "mail": "ohelbeck7@sun.com",
+    "password": "high-level",
+    "friend_ids": [
+  25,26
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "7575ee32-c98e-4e8f-bd45-20358a7587e7",
+    "first_name": "Jo",
+    "last_name": "Carver",
+    "country": "Colombia",
+    "profile_picture": "https://test.prophetmedia.pl/wp-content/uploads/2019/01/person2.jpg",
+    "mail": "jcarver8@infoseek.co.jp",
+    "password": "definition",
+    "friend_ids": [
+  0,10,13,18,19,22
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "2a818ac0-f86a-4929-9033-20d1bd215c64",
+    "first_name": "Trixy",
+    "last_name": "Ogle",
+    "country": "Colombia",
+    "profile_picture": "https://www.eatthis.com/wp-content/uploads/sites/4/2022/09/Prince-Charles-2022.jpg?quality=82&strip=1",
+    "mail": "togle9@engadget.com",
+    "password": "Focused",
+    "friend_ids": [
+  20,11,1,5,3,13
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "0760d7f8-daaf-4d07-9e1d-d84703d24c9d",
+    "first_name": "Trefor",
+    "last_name": "Kerby",
+    "country": "Colombia",
+    "profile_picture": "https://lingopolo.org/thai/sites/lingopolo.org.thai/files/styles/entry/public/images/2016/08/29/person-1911.jpg",
+    "mail": "tkerbya@shinystat.com",
+    "password": "tangible",
+    "friend_ids": [
+  15,11,12,13
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "30db2a37-7025-438c-aa8c-b13c3b48995c",
+    "first_name": "Dennis",
+    "last_name": "Beinke",
+    "country": "Greece",
+    "profile_picture": "https://i.insider.com/5d66d21e6f24eb396b6c8192?width=700",
+    "mail": "dbeinkeb@blogger.com",
+    "password": "emulation",
+    "friend_ids": [
+  7,8,9
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "8ee5b4b4-405e-437a-9ce6-0ba015e9805e",
+    "first_name": "Filippo",
+    "last_name": "Melmore",
+    "country": "Sweden",
+    "profile_picture": "https://madeofstill.com/wp-content/uploads/2022/04/signs-of-anointed-person.jpg",
+    "mail": "fmelmorec@elpais.com",
+    "password": "4th generation",
+    "friend_ids": [
+  10,11,12,13
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "add0a8dc-da6b-43b3-9459-1d2226508cf2",
+    "first_name": "Clyde",
+    "last_name": "Mooring",
+    "country": "Mongolia",
+    "profile_picture": "https://tlz.ae/wp-content/uploads/2023/01/One-person-LLC-Dubai-featured-image.png",
+    "mail": "cmooringd@census.gov",
+    "password": "maximized",
+    "friend_ids": [
+  15,11,12
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "1ed83de4-2496-4f96-883f-d891ebe2c2f9",
+    "first_name": "Trever",
+    "last_name": "Shillito",
+    "country": "China",
+    "profile_picture": "https://everydaypower.com/wp-content/uploads/2015/10/Become-A-More-Positive-Person-With-These-10-Actions.jpg",
+    "mail": "tshillitoe@state.gov",
+    "password": "encryption",
+    "friend_ids": [
+  1,8,9,19
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "77a97e29-b4d5-458d-a69a-6db84a2fc5e9",
+    "first_name": "Heidi",
+    "last_name": "Warlawe",
+    "country": "Russia",
+    "profile_picture": "https://gumlet.assettype.com/filmcompanion%2F2022-11%2F930182b8-0fc6-4ed3-9d16-7f71e256a987%2Fashok.png?format=auto",
+    "mail": "hwarlawef@seattletimes.com",
+    "password": "Visionary",
+    "friend_ids": [
+  23,12,0,11
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "c650f5e8-fc5b-48a2-95fb-2c0855580f58",
+    "first_name": "Ingeberg",
+    "last_name": "Cargenven",
+    "country": "Brazil",
+    "profile_picture": "https://media.glamourmagazine.co.uk/photos/623b3612980be8aafb01a665/4:3/w_1920,h_1440,c_limit/The%20Worst%20Person%20In%20The%20World%20230322GettyImages-1385537039_SQ.jpg",
+    "mail": "icargenveng@nasa.gov",
+    "password": "Synchronised",
+    "friend_ids": [
+  13,14,18
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "ea3f3aaf-ce06-42d6-8602-57c0cf027fea",
+    "first_name": "Abby",
+    "last_name": "Godney",
+    "country": "Poland",
+    "profile_picture": "https://www.lifeprint.com/asl101/signjpegs/f/farmer5.jpg",
+    "mail": "agodneyh@studiopress.com",
+    "password": "Programmable",
+    "friend_ids": [
+  20,10,0,18
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "eaccfdce-1795-4261-bf77-ddf56741c1b7",
+    "first_name": "Olivier",
+    "last_name": "Beckmann",
+    "country": "Philippines",
+    "profile_picture": "https://cdn.justjaredjr.com/wp-content/uploads/headlines/2022/07/mr-beast-becomes-2nd-person-to-reach-100-million-subscribers.jpg",
+    "mail": "obeckmanni@google.com.br",
+    "password": "product",
+    "friend_ids": [
+  13,15,19
+    ],
+    "chats": [
+  
+    ]
+  }, {
+    "id": "9f4a2bd5-f24c-4246-8d87-90c077f060f5",
+    "first_name": "Alberik",
+    "last_name": "Killiam",
+    "country": "Philippines",
+    "profile_picture": "https://www.essex.ac.uk/-/media/header-images/2022/03/boring-picture.jpg?h=512&w=512&la=en&hash=4D4BE6FD9477EAFFB5B5E76ADBDB34D9",
+    "mail": "akilliamj@webeden.co.uk",
+    "password": "directional",
+    "friend_ids": [
+  11,12,1,25
+    ],
+    "chats": [
+    ]
+  }
 ];
 
 export default userData;
