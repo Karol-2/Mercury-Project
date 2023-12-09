ee236698b6bcd254d68f06a6d0b5d83cb73e9fee
j.krol <j.krol@dev.polskiepolisy.pl>
2023-12-09 19:51:31 +0100

    Delete redux files

--- frontend/src/redux/actions/updateCallStatus.ts
+++
@@ -1,7 +0,0 @@
-import CallStatusState from "../../models/CallStatusState";
-export default (prop: keyof CallStatusState, value: any) => {
-  return {
-    type: "UPDATE_CALL_STATUS",
-    payload: { prop, value },
-  };
-};
