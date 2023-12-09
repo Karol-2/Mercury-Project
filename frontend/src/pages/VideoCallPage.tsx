596f77cdf5346daf6ed0407825b60b9e939fbf84
j.krol <j.krol@dev.polskiepolisy.pl>
2023-12-09 20:17:07 +0100

    Create negotiating function in meeting page

--- frontend/src/pages/VideoCallPage.tsx
+++ frontend/src/pages/VideoCallPage.tsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useUser } from "../helpers/UserProvider";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Socket } from "socket.io-client";
 function VideoCallPage() {
   const { user, userId } = useUser();
   const navigate = useNavigate();
   const localStream = useRef<HTMLVideoElement>(null);
   const remoteStream = useRef<HTMLVideoElement>(null);
+  const [makingOffer, setMakingOffer] = useState(false);
   useEffect(() => {
     if (userId === null) navigate("/login");
   }, [userId]);
@@ -17,6 +19,26 @@ function VideoCallPage() {
       return;
     }
   }, [user]);
+
+  useEffect(() => {
+    prepareWebRTC();
+  }, []);
+
+  async function prepareWebRTC() {
+
+  }
+
+  async function negotiate(peerConnection: RTCPeerConnection, socket: Socket) {
+    try {
+      setMakingOffer(true);
+      await peerConnection.setLocalDescription();
+      socket.emit("description", peerConnection.localDescription);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setMakingOffer(false);
+    }
+  }
 
   return (
     <>
