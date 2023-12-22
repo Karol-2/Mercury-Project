59439b0a0de96f5bca429676ccb135f7c3b9b7bf
gf-rog <gf.rogozinski@gmail.com>
2023-12-22 12:13:35 +0100

    Show messages on send

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,15 +1,27 @@
 import User from "../models/User";
 
 export interface MessageProps {
-  type: "sent" | "received" | "info",
-  author: User,
-  content: string
+  type: "sent" | "received" | "info";
+  author: User;
+  content: string;
 }
 
 function Message(props: MessageProps) {
-  const {type, author, content} = props
+  const { type, author, content } = props;
 
-  return <div>{content}</div>
+  let align = "";
+
+  if (type == "sent") {
+    align = "text-right";
+  } else if (type == "received") {
+    align = "text-left";
+  }
+
+  return (
+    <div className={`bg-my-light text-my-dark h-20 my-2 ${align}`}>
+      {content}
+    </div>
+  );
 }
 
 export default Message;
