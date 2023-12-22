b36ae3646cb799a1b51b17c8e4be502f38b2c4d5
gf-rog <gf.rogozinski@gmail.com>
2023-12-22 12:13:35 +0100

    Show messages on send

--- frontend/src/components/ChatBox.tsx
+++ frontend/src/components/ChatBox.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
-import { MessageProps } from "./Message";
+import React, { useRef, useState } from "react";
+import Message, { MessageProps } from "./Message";
 import User from "../models/User";
 
 function ChatBox() {
+  const [messages, setMessages] = useState<MessageProps[]>([]);
   const enterPressed = useRef<boolean>(false);
-  
+
   const handleKeyPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key != "Enter") return;
     if (e.shiftKey) return;
@@ -16,27 +17,33 @@ function ChatBox() {
       if (!text) return;
 
       sendMessage({} as User, text);
-      e.currentTarget.value = ""
+      e.currentTarget.value = "";
     }
-  }
+  };
 
   const handleKeyRelease = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key != "Enter") return;
     enterPressed.current = false;
     e.preventDefault();
-  }
+  };
 
   const sendMessage = (author: User, content: string) => {
-    const message: MessageProps = {type: "sent", author, content}
-    console.log("SEND MESSAGE:", message)
-  }
+    const message: MessageProps = { type: "sent", author, content };
+    setMessages([...messages, message]);
+    console.log("SEND MESSAGE:", message);
+  };
 
   const receiveMessage = (message: MessageProps) => {
-    console.log("RECEIVE MESSAGE:", message)
-  }
+    console.log("RECEIVE MESSAGE:", message);
+  };
 
   return (
     <div className="w-96">
+      <div className="flex flex-col">
+        {messages.map((e, i) => (
+          <Message key={i} {...e} />
+        ))}
+      </div>
       <textarea
         className="text-my-dark form-input"
         rows={4}
