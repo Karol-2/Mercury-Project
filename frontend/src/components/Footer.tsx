9bed6fe7c57e7963fa4b91b2ea2d2f43f3044de7
gf-rog <gf.rogozinski@gmail.com>
2023-12-24 11:05:06 +0100

    Extract common style to variable

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -15,25 +15,27 @@ export default function Footer() {
     });
   };
 
+  const socialStyle = "flex flex-row gap-1 font-medium text-my-darker clickable"
+
   return (
     <footer className="bg-my-orange mt-5 text-my-light p-10 flex flex-row">
       <section className="min-w-full">
         <div className="mx-50 lg:mx-72 flex flex-row justify-around">
           <figure className="flex flex-col content-evenly gap-3">
-            <div className="flex flex-row gap-1 font-medium text-my-darker clickable">
+            <div className={socialStyle}>
               <FontAwesomeIcon icon={faYoutube} className="text-3xl" />
               <span className="self-center">Mercury App Channel</span>
             </div>
-            <div className="flex flex-row gap-1 font-medium text-my-darker clickable">
+            <div className={socialStyle}>
               <FontAwesomeIcon icon={faFacebook} className="text-3xl" />
               <span className="self-center">MercuryApp</span>
             </div>
-            <div className="flex flex-row gap-1 font-medium text-my-darker clickable">
+            <div className={socialStyle}>
               <FontAwesomeIcon icon={faTwitter} className="text-3xl" />
               <span className="self-center">@MercuryApp</span>
             </div>
             <a
-              className="flex flex-row gap-1 font-medium text-my-darker clickable"
+              className={socialStyle}
               href="https://github.com/Karol-2/Mercury-Project"
             >
               <FontAwesomeIcon icon={faGithub} className="text-3xl" />
