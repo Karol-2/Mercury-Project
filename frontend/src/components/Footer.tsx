7d2053bd1293d69fcf2c209022fab7ffb9a63b0e
gf-rog <gf.rogozinski@gmail.com>
2023-12-24 10:42:49 +0100

    Remove redundant whitespace

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -16,41 +16,40 @@ export default function Footer() {
   };
 
   return (
-    <footer className="  bg-my-orange mt-5 text-my-light p-10 flex flex-row">
-      <section className=" min-w-full">
+    <footer className="bg-my-orange mt-5 text-my-light p-10 flex flex-row">
+      <section className="min-w-full">
         <div className="mx-50 lg:mx-72 flex flex-row justify-around">
-          <figure className=" flex flex-col content-evenly gap-3">
-            <div className=" flex flex-row font-medium text-my-darker clickable">
-              <FontAwesomeIcon icon={faYoutube} className=" text-3xl " />{" "}
-              <p className=" self-center">Mercury App Channel</p>
+          <figure className="flex flex-col content-evenly gap-3">
+            <div className="flex flex-row font-medium text-my-darker clickable">
+              <FontAwesomeIcon icon={faYoutube} className="text-3xl" />
+              <p className="self-center">Mercury App Channel</p>
             </div>
-            <figure className=" flex flex-row font-medium text-my-darker clickable">
-              <FontAwesomeIcon icon={faFacebook} className=" text-3xl" />{" "}
+            <figure className="flex flex-row font-medium text-my-darker clickable">
+              <FontAwesomeIcon icon={faFacebook} className="text-3xl" />
               <p>MercuryApp</p>
             </figure>
-            <figure className=" flex flex-row font-medium text-my-darker clickable">
-              <FontAwesomeIcon icon={faTwitter} className=" text-3xl" />{" "}
+            <figure className="flex flex-row font-medium text-my-darker clickable">
+              <FontAwesomeIcon icon={faTwitter} className="text-3xl" />
               <p>@MercuryApp</p>
             </figure>
             <a
-              className=" flex flex-row font-medium text-my-darker clickable"
+              className="flex flex-row font-medium text-my-darker clickable"
               href="https://github.com/Karol-2/Mercury-Project"
             >
-              <FontAwesomeIcon icon={faGithub} className=" text-3xl" />{" "}
+              <FontAwesomeIcon icon={faGithub} className="text-3xl" />
               <p>Mercury Project</p>
             </a>
           </figure>
 
           <div className="flex flex-col">
-            <button onClick={scrollToTop} className=" text-my-darker clickable">
+            <button onClick={scrollToTop} className="text-my-darker clickable">
               <FontAwesomeIcon
                 icon={faArrowCircleUp}
-                className=" text-7xl self-end"
-              />{" "}
+                className="text-7xl self-end"
+              />
             </button>
 
-            <p className=" font-thin mt-20">
-              {" "}
+            <p className="font-thin mt-20">
               &#169; 2023 Mercury. All Rights Reserved.
             </p>
           </div>
