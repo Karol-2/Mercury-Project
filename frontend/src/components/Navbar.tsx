fd6c785478888bbc14ad715b5f3ad36f3a4e78d7
gf-rog <gf.rogozinski@gmail.com>
2023-12-24 12:37:46 +0100

    Refactor dropdown menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -23,59 +23,56 @@ function Navbar() {
     { to: "/messages", text: "Messages" },
   ];
 
+  const menuPosition = "absolute right-1 top-24";
+  const menuColor = "shadow-xl bg-my-orange";
+
+  const linkStyle =
+    "whitespace-nowrap hover:bg-my-purple rounded-sm p-2 w-full text-center";
+
   return (
-    <nav className="bg-my-dark py-2 flex flex-col sm:flex-row justify-between items-center">
+    <nav className="bg-my-dark py-2 px-5 flex flex-col sm:flex-row justify-between items-center">
       <div className="flex flex-row select-none">
-        <img src={LogoSVG} alt="Mercury Logo" className="h-20 w-20 pr-5 ml-5" />
+        <img src={LogoSVG} alt="Mercury Logo" className="h-20 w-20 pr-5" />
         <span className="self-center text-my-orange font-bold text-xl md:text-2xl">
           Mercury
         </span>
       </div>
       {user && (
-        <div className="self-center flex items-center">
+        <div className="h-24 self-center flex gap-4 items-center">
           {navLinks.map((link) => (
             <Link
               key={link.to}
               to={link.to}
-              className="p-5 rounded-lg transition duration-250 ease-in-out mr-5 hover:bg-my-orange font-bold text-lg"
+              className="p-5 rounded-lg transition duration-250 ease-in-out hover:bg-my-orange font-bold text-lg"
             >
               {link.text}
             </Link>
           ))}
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="rounded-lg transition duration-250 ease-in-out mr-5 font-bold text-lg p-5 hover:bg-my-orange"
+            className="rounded-lg w-20 h-20 transition duration-250 ease-in-out font-bold text-lg p-3.5 hover:bg-my-orange"
           >
             <img
               src={user.profile_picture}
-              className="rounded-full w-12 h-12 border-my-orange border-2 object-cover "
+              className="rounded-full w-full h-full border-my-orange border-2 object-cover"
             />
-            {isOpen && (
-              <div
-                id="dropdown-menu"
-                className="shadow-xl absolute top-40 right-18 md:top-20 md:right-0 flex flex-col items-start rounded-lg bg-my-orange py-2 mr-5 font-normal text-base"
-              >
-                <Link
-                  to={"/profile"}
-                  className="whitespace-nowrap mt-2 hover:bg-my-purple p-2 w-full"
-                >
-                  My profile
-                </Link>
-                <Link
-                  to={"/friends"}
-                  className="mt-2 hover:bg-my-purple p-2 w-full"
-                >
-                  Friends
-                </Link>
-                <button
-                  onClick={handleLogout}
-                  className="mt-2 hover:bg-my-purple p-2 w-full"
-                >
-                  Logout
-                </button>
-              </div>
-            )}
           </button>
+          {isOpen && (
+            <div
+              id="dropdown-menu"
+              className={`${menuPosition} ${menuColor} w-28 p-2 flex flex-col rounded-lg`}
+            >
+              <Link to={"/profile"} className={linkStyle}>
+                My profile
+              </Link>
+              <Link to={"/friends"} className={linkStyle}>
+                Friends
+              </Link>
+              <button onClick={handleLogout} className={linkStyle}>
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       )}
     </nav>
