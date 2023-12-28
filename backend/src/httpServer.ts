78aaf7f00e76633b4f736a85032833f2ec63c67d
j.krol <j.krol@dev.polskiepolisy.pl>
2023-12-28 13:48:11 +0100

    Add chat router

--- backend/src/httpServer.ts
+++ backend/src/httpServer.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import servers from "./server";
 import usersRouter from "./routes/usersRoute";
 import authRouter from "./routes/authRoute";
+import chatRouter from "./routes/chatRoute";
 import importInitialData from "./data/importData";
 const { app } = servers;
 
@@ -11,3 +12,4 @@ importInitialData().then((res) => console.log(res));
 
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
+app.use("/chat", chatRouter);
---
+++ backend/src/routes/chatRoute.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const chatRouter = Router();
+
+export default chatRouter;
