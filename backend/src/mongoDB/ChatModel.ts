0b9c326e883c681585317b7b3614a7be51ebead1
j.krol <j.krol@dev.polskiepolisy.pl>
2023-12-28 13:43:14 +0100

    Update chat model

--- backend/src/mongoDB/ChatModel.ts
+++ backend/src/mongoDB/ChatModel.ts
@@ -1,8 +1,10 @@
 import { model, Schema } from "mongoose";
 import Chat from "../models/Chat";
 const chatSchema = new Schema<Chat>({
-    userIds: [],
-    messages: []
+    authorId: {type: String, required: true},
+    receiverId: {type: String, required: true},
+    content: {type: String, required: true},
+    created_date: {type: Date, required: true},
 });
 
 const ChatModel = model("Chat", chatSchema, "chats");
