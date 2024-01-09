94031bfb9fdf52a5d4efb56ca91d7410502cc1c8
gf-rog <gf.rogozinski@gmail.com>
2024-01-09 13:06:23 +0100

    Add isFriend function

---
+++ backend/src/users.ts
@@ -0,0 +1,22 @@
+import { Session } from "neo4j-driver";
+
+export async function isFriend(
+  session: Session,
+  firstUserId: string,
+  secondUserId: string,
+) {
+  try {
+    const request = await session.run(
+      `
+      MATCH (u1:User {id: $firstUserId})
+      MATCH (u2:User {id: $secondUserId})
+      RETURN exists((u1)-[:IS_FRIENDS_WITH]-(u2))
+      `,
+      { firstUserId, secondUserId },
+    );
+
+    return request.records.length > 0
+  } catch (err) {
+    return false;
+  }
+}
