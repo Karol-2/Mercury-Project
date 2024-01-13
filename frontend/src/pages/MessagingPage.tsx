680661cbbeb59b430c7cdc05fd1a3a3614acfe03
gf-rog <gf.rogozinski@gmail.com>
2024-01-13 18:25:20 +0100

    Redirect to login page if not logged in

--- frontend/src/pages/MessagingPage.tsx
+++ frontend/src/pages/MessagingPage.tsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useLayoutEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import Navbar from "../components/Navbar";
@@ -11,24 +12,31 @@ import { RootState } from "../redux/store";
 import User from "../models/User";
 
 function MessagingPage() {
+  const navigate = useNavigate();
   const friends = useSelector((state: RootState) => state.friends);
   const { friendId } = useParams();
   const friend = friends.find((f: User) => f.id === friendId);
   const { user, socket } = useUser();
 
-  if (!user || !socket || !friendId) {
-    return <PageNotFound />;
-  }
+  useEffect(() => {
+    if (user === null) {
+      navigate("/login");
+    }
+  }, [user]);
+
+  const showChatBox = user && socket && friendId;
 
   return (
     <>
       <Navbar />
-      <ChatBox
-        user={user}
-        socket={socket}
-        friendId={friendId}
-        friend_profile_picture={friend.profile_picture}
-      />
+      {showChatBox ? (
+        <ChatBox
+          user={user}
+          socket={socket}
+          friendId={friendId}
+          friend_profile_picture={friend.profile_picture}
+        />
+      ) : null}
       <Footer />
     </>
   );
