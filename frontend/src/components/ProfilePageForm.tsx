d03e1b98a357f2863b1410133b7b7d198e63d2d5
gf-rog <gf.rogozinski@gmail.com>
2024-01-13 19:47:09 +0100

    Remove duplicate buttons

--- frontend/src/components/ProfilePageForm.tsx
+++ frontend/src/components/ProfilePageForm.tsx
@@ -118,44 +118,6 @@ function ProfilePageForm(props: ProfilePageFormProps) {
               Edit
             </button>
           )}
-          <button onClick={deleteUser} className="btn secondary">
-            Remove account
-          </button>
-
-          {showDeleteModal && (
-            <Modal
-              text={`Are you sure that you want to delete you account?`}
-              handleYes={deleteUser}
-              handleNo={() => setShowDeleteModal(false)}
-            ></Modal>
-          )}
-        </div>
-        <div className="my-5">
-          <h2 className="text-4xl font-extrabold dark:text-white">Friends</h2>
-          <ul className="list-disc">
-            {friends.map((friend) => (
-              <li className="my-1" key={friend.id}>
-                {`${friend.first_name} ${friend.last_name}`}
-                <button
-                  className="mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                  onClick={() => joinMeeting(friend.id)}
-                >
-                  Meeting
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="my-5">
-          {isEditing ? (
-            <button onClick={handleSaveClick} className="btn primary">
-              Save
-            </button>
-          ) : (
-            <button onClick={handleEditClick} className="btn primary">
-              Edit
-            </button>
-          )}
           <button onClick={createMeeting} className="btn secondary">
             Create a meeting
           </button>
