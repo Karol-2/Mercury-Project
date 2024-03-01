9005977e08f049f9abe7471359d777a6cc584d1d
j.krol <janek01krol@gmail.com>
2024-03-01 18:19:38 +0100

    Implement call buttons

--- frontend/src/pages/VideoCallPage.tsx
+++ frontend/src/pages/VideoCallPage.tsx
@@ -7,17 +7,19 @@ import { Socket } from "socket.io-client";
 import stunServers from "../stun/stunServers";
 import Meeting from "../models/Meeting";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhoneSlash } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneSlash, faMicrophone, faMicrophoneSlash, faVideo, faVideoSlash } from "@fortawesome/free-solid-svg-icons";
 
 function VideoCallPage() {
   const { userId, socket, meeting, leaveMeeting } = useUser();
+  const [stream, setStream] = useState<MediaStream | null>(null);
   const firstRefresh = useRef<boolean>(true);
   const navigate = useNavigate();
   const localStream = useRef<HTMLVideoElement>(null);
   const remoteStream = useRef<HTMLVideoElement>(null);
   const [makingOffer, setMakingOffer] = useState(false);
+  const [audio, setAudio] = useState(true);
+  const [video, setVideo] = useState(true);
   const peerConnectionRef = useRef<RTCPeerConnection | null>(null);
-
   const getPeerConnection = () => peerConnectionRef.current;
 
   useEffect(() => {
@@ -40,6 +42,31 @@ function VideoCallPage() {
     }
   }, []);
 
+  async function startStopAudio() {
+    if (stream) {
+      const tracks = stream.getAudioTracks();
+      if (audio) {
+        setAudio(false);
+        tracks.forEach(track => track.enabled = false);
+      } else {
+        setAudio(true);
+        tracks.forEach(track => track.enabled = true);
+      }
+    }
+  }
+  async function startStopVideo() {
+    if (stream) {
+      const tracks = stream.getVideoTracks();
+      if (video) {
+        setVideo(false);
+        tracks.forEach(track => track.enabled = false);
+      } else {
+        setVideo(true);
+        tracks.forEach(track => track.enabled = true);
+      }
+    }
+  }
+
   async function prepareWebRTC(
     socket: Socket,
     meeting: Meeting,
@@ -52,6 +79,7 @@ function VideoCallPage() {
         audio: true,
         video: true,
       });
+      setStream(stream);
       stream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, stream);
       });
@@ -170,6 +198,18 @@ function VideoCallPage() {
         </div>
       </div>
       <div className=" flex justify-center">
+        <button className="btn p-6" onClick={startStopAudio}>
+          {audio 
+            ? <FontAwesomeIcon icon={faMicrophone} />
+            : <FontAwesomeIcon icon={faMicrophoneSlash} />
+          }
+        </button>
+        <button className="btn p-6" onClick={startStopVideo}>
+          {video 
+            ? <FontAwesomeIcon icon={faVideo} /> 
+            : <FontAwesomeIcon icon={faVideoSlash} />
+          }
+        </button>
         <button
           onClick={() => handleLeaveMeeting()}
           className="btn bg-my-red p-6"
