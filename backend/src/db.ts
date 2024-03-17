e867c447f39a5f1b2dbd9dcec25d5ba7db88cde9
gf-rog <gf.rogozinski@gmail.com>
2024-03-17 13:07:27 +0100

    Use createUser function

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,7 @@
-import { v4 as uuidv4 } from "uuid";
-
 import driver from "./driver/driver";
-import bcrypt from "bcrypt";
 
 import userData from "./data/users";
-import wordToVec from "./misc/wordToVec";
+import { createUser } from "./users";
 
 export async function isDatabaseEmpty() {
   const session = driver.session();
@@ -32,28 +29,11 @@ export async function importInitialData() {
   try {
     const userIds: string[] = [];
 
-    // Create users
-    const createUserQuery = `
-      CREATE (u:User $user)
-    `;
-
     for (const user of userData) {
-      const userId = uuidv4();
-      userIds.push(userId);
-
-      const userClean: any = Object.assign({}, user);
-      delete userClean.friend_ids;
-      delete userClean.chats;
-
-      userClean.id = userId;
-      userClean.name_embedding = wordToVec(
-        userClean.first_name + userClean.last_name,
-      );
-      const { password } = userClean;
-      const passwordHashed = await bcrypt.hash(password, 10);
-      userClean.password = passwordHashed;
-
-      await session.run(createUserQuery, { user: userClean });
+      const newUser = await createUser(session, user);
+      if (!("errors" in newUser)) {
+        userIds.push(newUser.id);
+      }
     }
 
     // Create relationships
