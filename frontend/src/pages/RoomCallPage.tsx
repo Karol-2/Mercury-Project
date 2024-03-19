d9ff9d8d64aa35d2a0e0f46fca9dfbe8b8d4d433
j.krol <janek01krol@gmail.com>
2024-03-19 20:35:40 +0100

    Install peerjs package at create peer

--- frontend/src/pages/RoomCallPage.tsx
+++ frontend/src/pages/RoomCallPage.tsx
@@ -1,7 +1,11 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import peer from "../peer/peer";
 
 function RoomCallPage() {
+    peer.on("call", (call) => {
+        console.log(call);
+    })
     return (
         <>
             <Navbar />
---
+++ frontend/src/peer/peer.ts
@@ -0,0 +1,8 @@
+import Peer from "peerjs";
+
+const peer = new Peer({
+    host: "/",
+    port: 8000
+});
+
+export default peer;
