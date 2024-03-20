f35b18b81303832c4b47440caeb6f1062e1857b6
gf-rog <gf.rogozinski@gmail.com>
2024-03-20 19:12:41 +0100

    Fix unauthorized after token expired

--- backend/src/kcAdminClient.ts
+++ backend/src/kcAdminClient.ts
@@ -5,23 +5,28 @@ const kcAdminClient = new KeycloakAdminClient({
   realmName: "mercury",
 });
 
-try {
-  await kcAdminClient.auth({
-    grantType: "client_credentials",
-    clientId: "mercury-backend",
-    clientSecret: process.env.CLIENT_SECRET,
-  });
-} catch (e: any) {
-  if (!e || !e.cause) {
-    throw e;
-  }
+async function authAdminClient() {
+  try {
+    await kcAdminClient.auth({
+      grantType: "client_credentials",
+      clientId: "mercury-backend",
+      clientSecret: process.env.CLIENT_SECRET,
+    });
+  } catch (e: any) {
+    if (!e || !e.cause) {
+      throw e;
+    }
 
-  if (["address", "port", "code"].every((key) => key in e.cause)) {
-    const { address, port, code } = e.cause;
-    console.log("Can't connect to Keycloak: ", { address, port, code });
-  } else {
-    console.error("Can't connect to Keycloak:", e);
+    if (["address", "port", "code"].every((key) => key in e.cause)) {
+      const { address, port, code } = e.cause;
+      console.log("Can't connect to Keycloak: ", { address, port, code });
+    } else {
+      console.error("Can't connect to Keycloak:", e);
+    }
   }
 }
+
+await authAdminClient();
+setInterval(() => authAdminClient(), 58 * 1000);
 
 export default kcAdminClient;
