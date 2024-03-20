a3b525b9cb51b4ef4cbb8c7a5723a0cff54889b3
j.krol <janek01krol@gmail.com>
2024-03-20 19:35:35 +0100

    Delete unused properties from Notifications Page

--- frontend/src/pages/NotificationsPage.tsx
+++ frontend/src/pages/NotificationsPage.tsx
@@ -1,14 +1,13 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useEffect, useState } from "react";
-import dataService from "../services/data";
+import { useEffect } from "react";
 import { useUser } from "../helpers/UserProvider";
 import RoomNotification from "../models/RoomNotification";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import addNotification from "../redux/actions/addNotification";
 function NotificationsPage() {
-    const {userId, socket} = useUser();
+    const {socket} = useUser();
     const dispatch = useDispatch();
     const roomNotifications: RoomNotification[] = useSelector((state: RootState) => state.notifications);
     useEffect(() => {
