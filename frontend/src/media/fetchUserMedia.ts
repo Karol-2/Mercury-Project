ef93ab242e3ecf93566399ee92aca696fdb45afb
j.krol <janek01krol@gmail.com>
2024-03-20 17:22:24 +0100

    Create fetchUserMedia function

---
+++ frontend/src/media/fetchUserMedia.ts
@@ -0,0 +1,9 @@
+async function fetchUserMedia() {
+    const localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    });
+    return localStream;
+}
+
+export default fetchUserMedia;
