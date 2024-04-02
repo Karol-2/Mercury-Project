9d5a72f7baaefd09182ede5f51b43249ef6819df
j.krol <janek01krol@gmail.com>
2024-04-02 18:03:51 +0200

    Fix peer problems in notifications page

--- frontend/src/pages/NotificationsPage.tsx
+++ frontend/src/pages/NotificationsPage.tsx
@@ -1,27 +1,28 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useUser } from "../helpers/UserProvider";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
 import dataService from "../services/data";
 import { useNavigate } from "react-router-dom";
-import Peer from "peerjs";
 import Notification from "../components/Notification";
 import { useEffect } from "react";
 function NotificationsPage() {
-  const { deleteNotification, notifications, socket, userId, user } = useUser();
+  const { peer, deleteNotification, notifications, socket, userId, user } = useUser();
   const navigate = useNavigate();
-  const peer: Peer = useSelector((state: RootState) => state.peer);
   const handleRoomInvite = async (roomId: string) => {
-    console.log("[HANDLE ROOM INVITE]: starts");
-    await dataService.fetchData(`/room/${roomId}`, "DELETE", {});
-    console.log("[HANDLE ROOM INVITE]: delete invite from DB");
-    const fullName = `${user?.first_name} ${user?.last_name}`;
-    console.log("[HANDLE ROOM INVITE]: create full name: ", fullName);
-    console.log("[PEER]: ", peer);
-    socket?.emit("joinRoom", { roomId, peerId: peer.id, userId, fullName });
-    console.log("[HANDLE ROOM INVITE]: emitted joinRoom");
-    navigate(`/room/${roomId}`);
+    if (peer) {
+      console.log("[HANDLE ROOM INVITE]: starts");
+      await dataService.fetchData(`/room/${roomId}`, "DELETE", {});
+      console.log("[HANDLE ROOM INVITE]: delete invite from DB");
+      const fullName = `${user?.first_name} ${user?.last_name}`;
+      console.log("[HANDLE ROOM INVITE]: create full name: ", fullName);
+      console.log("[PEER]: ", peer);
+      socket?.emit("joinRoom", { roomId, peerId: peer.id, userId, fullName });
+      console.log("[HANDLE ROOM INVITE]: emitted joinRoom");
+      navigate(`/room/${roomId}`);
+      return;
+    }
+    navigate("/profile");
+    return;
   };
 
   const declineInvite = (inviteId: string) => {
