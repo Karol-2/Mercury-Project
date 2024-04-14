dbc8430284d8c9527ac10fe021a068a4dd884cc4
j.krol <janek01krol@gmail.com>
2024-04-14 18:59:37 +0200

    Implement joining to a call via notification

--- frontend/src/pages/NotificationPage.tsx
+++ frontend/src/pages/NotificationPage.tsx
@@ -1,8 +1,23 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useUser } from "../helpers/UserContext";
+import { useMeeting } from "../helpers/MeetingProvider";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function NotificationPage() {
+    const navigate = useNavigate();
     const {notifications} = useUser();
+    const { joinMeeting, meeting } = useMeeting();
+    useEffect(() => {
+        if (meeting?.id) {
+          navigate("/meeting");
+        }
+      }, [meeting]);
+    const invokeAction = (notification: any) => {
+        if (notification.type === "call") {
+            joinMeeting(notification.senderId)
+        }
+    }
     return (
         <>
             <Navbar />
@@ -10,6 +25,7 @@ function NotificationPage() {
                 {notifications.map((n,i) => <div key={i}>
                     <span>type: {n.type} </span>
                     <span>from: {n.senderId}</span>
+                    <button onClick={() => invokeAction(n)}>action</button>
                 </div>)}
             </div>
             <Footer />
