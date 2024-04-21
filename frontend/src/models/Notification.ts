d8b256b0486a1342696dd5dcfbe7e64210c3f5f8
j.krol <janek01krol@gmail.com>
2024-04-21 20:23:35 +0200

    Create Notification interface

---
+++ frontend/src/models/Notification.ts
@@ -0,0 +1,14 @@
+type NotificationActionType = 
+    ((friendId: string) => Promise<string | void>) | 
+    ((userId: string) => void);
+
+interface Notification {
+    id?: string;
+    type: "message" | "friend" | "call";
+    senderId: string;
+    senderFullName: string;
+    action?: NotificationActionType;
+    deleteNotification?: (id: string) => void;
+}
+
+export default Notification;
