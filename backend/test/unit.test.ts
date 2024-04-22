a9ee12c662d1c49dcf519e5de244017f15d1a66d
Piotr Maszczak <piotrmaszczak25@gmail.com>
2024-04-22 17:32:04 +0200

    Add word difference unit test

--- backend/test/unit.test.ts
+++ backend/test/unit.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import letterToKb from "../src/misc/wordToVec.js";
+import letterToKb, { wordDifference } from "../src/misc/wordToVec.js";
 
 test("Letter to Kb", async () => {
   expect(letterToKb("X")).toStrictEqual([
@@ -22,4 +22,13 @@ test("Letter to Kb", async () => {
 
   expect(letterToKb("1")).toStrictEqual([]);
   expect(letterToKb(" ")).toStrictEqual([]);
+});
+
+test("Word Difference", async () => {
+  expect(wordDifference("a", "b")).toStrictEqual(-5.952380952380951);
+  expect(wordDifference("Adam", "Padam")).toBeGreaterThan(1);
+  expect(wordDifference("John", "John")).toBeGreaterThan(2.9);
+  expect(
+    wordDifference("Really long text idk", "Dinosaurs with mexican hats"),
+  ).toBeNaN();
 });
